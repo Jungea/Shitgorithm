eb07d875740c712bb98d11afc8a19c7ea123354d
Jungea <jungea2241@gmail.com>
2025-08-30 23:50:47 +0900

    ✅ [PGS] 42889.실패율 재풀이

--- src/PGS/PGS42889.java
+++ src/PGS/PGS42889.java
@@ -7,8 +7,62 @@ import java.util.List;
 
 public class PGS42889 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3})));
+        System.out.println(Arrays.toString(solution1(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3})));
     }
+
+    public static class StageInfo implements Comparable<StageInfo> {
+        int no;
+        int tryCount;
+        int finishCount;
+
+        public StageInfo(int no, int tryCount, int finishCount) {
+            this.no = no;
+            this.tryCount = tryCount;
+            this.finishCount = finishCount;
+        }
+
+        public float getPercent() {
+            if (tryCount == 0) return 0;
+
+            return (float) tryCount / (tryCount + finishCount);
+        }
+
+        @Override
+        public int compareTo(StageInfo o) {
+            if (this.getPercent() == o.getPercent()) {
+                return this.no - o.no;
+            } else {
+                return -1 * Float.compare(getPercent(), o.getPercent());
+            }
+        }
+    }
+
+    private static int[] solution1(int N, int[] stages) {
+
+        // 도달 스테이지 카운트
+        int[] count = new int[N + 2];
+        for (int stage : stages) {
+            count[stage]++;
+        }
+        System.out.println(Arrays.toString(count));
+
+        List<StageInfo> list = new ArrayList<>(); // stage, 실패율
+        int finish = count[N + 1];
+        for (int i = N; i > 0; i--) {
+            list.add(new StageInfo(i, count[i], finish));
+            finish += count[i];
+        }
+
+        list.sort(null);
+
+        int[] answer = new int[N];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i).no;
+        }
+
+        return answer;
+    }
+
 
     public static int[] solution(int N, int[] stages) {
         int[] answer = new int[N + 1];
