9e52390ecfa27f73a0eef8a34b2ff69d33f9c240
Jungea <jungea2241@gmail.com>
2025-08-30 23:59:47 +0900

    ✅ [PGS] 42889.실패율 재풀이

--- src/PGS/PGS42889.java
+++ src/PGS/PGS42889.java
@@ -2,55 +2,50 @@ package PGS;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class PGS42889 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution1(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3})));
+        System.out.println(Arrays.toString(solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3})));
     }
 
-    public static class StageInfo implements Comparable<StageInfo> {
+    static class Stage implements Comparable<Stage> {
         int no;
         int tryCount;
-        int finishCount;
+        int totalCount;
 
-        public StageInfo(int no, int tryCount, int finishCount) {
+        public Stage(int no, int tryCount, int totalCount) {
             this.no = no;
             this.tryCount = tryCount;
-            this.finishCount = finishCount;
+            this.totalCount = totalCount;
         }
 
         public float getPercent() {
-            if (tryCount == 0) return 0;
-
-            return (float) tryCount / (tryCount + finishCount);
+            return tryCount == 0 ? 0 : (float) tryCount / totalCount;
         }
 
         @Override
-        public int compareTo(StageInfo o) {
-            if (this.getPercent() == o.getPercent()) {
-                return this.no - o.no;
-            } else {
-                return -1 * Float.compare(getPercent(), o.getPercent());
-            }
+        public int compareTo(Stage o) {
+            return this.getPercent() == o.getPercent() ?
+                    this.no - o.no : Float.compare(o.getPercent(), this.getPercent());
         }
     }
 
-    private static int[] solution1(int N, int[] stages) {
+    public static int[] solution(int N, int[] stages) {
 
         // 도달 스테이지 카운트
         int[] count = new int[N + 2];
+        int totalCount = 0;
         for (int stage : stages) {
             count[stage]++;
+            totalCount++;
         }
         System.out.println(Arrays.toString(count));
 
-        List<StageInfo> list = new ArrayList<>(); // stage, 실패율
-        int finish = count[N + 1];
-        for (int i = N; i > 0; i--) {
-            list.add(new StageInfo(i, count[i], finish));
-            finish += count[i];
+        List<Stage> list = new ArrayList<>(); // stage, 실패율
+        for (int i = 1; i < N + 1; i++) {
+            list.add(new Stage(i, count[i], totalCount));
+            totalCount -= count[i];
         }
 
         list.sort(null);
@@ -61,60 +56,5 @@ public class PGS42889 {
         }
 
         return answer;
-    }
-
-
-    public static int[] solution(int N, int[] stages) {
-        int[] answer = new int[N + 1];
-
-        int total = 0;
-        for (int i = 0; i < stages.length; i++) {
-            total += 1;
-
-            if (stages[i] == N + 1)
-                continue;
-
-            answer[stages[i]] += 1;
-        }
-
-        List<Stage> list = new ArrayList<>();
-        for (int i = 1; i < answer.length; i++) {
-            list.add(new Stage(i, (float) answer[i] / total));
-            total -= answer[i];
-        }
-
-        Collections.sort(list);
-
-        int[] result = new int[N];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = list.get(i).getNum();
-        }
-
-        return result;
-    }
-
-    public static class Stage implements Comparable {
-        private int num;
-        private float fail;
-
-        public Stage(int num, float fail) {
-            this.num = num;
-            this.fail = fail;
-        }
-
-        public int getNum() {
-            return num;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            Stage s = (Stage) o;
-
-            if (this.fail > s.fail)
-                return -1;
-            else if (this.fail < s.fail)
-                return 1;
-            return this.num - s.num;
-        }
     }
 }
