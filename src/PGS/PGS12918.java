7df1400b2c7f1b51fb30f4c9476e728344ae32d8
Jungea <jungea2241@gmail.com>
2025-08-30 20:15:49 +0900

    ✅ [BOJ] 12918. 문자열 다루기 기본

--- src/PGS/PGS12918.java
+++ src/PGS/PGS12918.java
@@ -4,11 +4,28 @@ import java.util.regex.Pattern;
 
 public class PGS12918 {
     public static void main(String[] args) {
-        System.out.println(solution("1234"));
+        System.out.println(solution1("1234"));
         System.out.println(solution("s123"));
     }
 
-    public static boolean solution(String s) {
-        return Pattern.matches("[0-9]{4}|[0-9]{6}", s);
+    private static boolean solution1(String s) {
+        if (s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
     }
+
+    public static boolean solution(String s) {
+//        return Pattern.matches("[0-9]{4}|[0-9]{6}", s);
+        return s.matches("\\d{4}|\\d{6}");
+    }
+
+
 }
