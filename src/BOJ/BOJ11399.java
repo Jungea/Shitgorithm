29c530866d1689a137161897a98dbec7da6aa4b3
Jungea <jungea2241@gmail.com>
2025-11-27 17:59:28 +0900

    ✅ [BOJ] 11399. ATM

---
+++ src/BOJ/BOJ11399.java
@@ -0,0 +1,40 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ11399 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int sum = solution1(s, n);
+
+        System.out.println(sum);
+    }
+
+    private static int solution1(String s, int n) {
+        String[] splitString = s.split(" ");
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(splitString[i]);
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        int add = 0;
+        for (int i = 0; i < n; i++) {
+            sum += (arr[i] + add);
+            add += arr[i];
+        }
+        return sum;
+    }
+
+
+}
